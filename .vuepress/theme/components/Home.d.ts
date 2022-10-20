644e8112e98171f214f8dca58991eb66c072c8f3
zhangxu <nikolazhang@163.com>
2022-10-20 13:11:10 +0800

    remove files

--- .vuepress/theme/components/Home.d.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-interface ActionConfig {
-    text: string;
-    link: string;
-}
-declare const _default: import("vue/types/vue").ExtendedVue<Vue, unknown, unknown, {
-    actionLinks: ActionConfig[];
-}, Record<never, any>>;
-export default _default;
