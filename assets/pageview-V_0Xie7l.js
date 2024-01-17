408de8a197476eefbfc170199c92106b07f6d3be
NikolaZhang <NikolaZhang@163.com>
2024-01-17 07:14:10 +0000

    Deploying to gh-pages from @ NikolaZhang/nikolazhang.github.io@7aca8226268667b11d4dff5dcf1845dc974c14fe 🚀

---
+++ assets/pageview-V_0Xie7l.js
@@ -0,0 +1,7 @@
+import{_ as o}from"./app-MeKfRsNW.js";var n={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"http://comment.nikolazhang.top/",meta:["nick","mail","link"],requiredMeta:["nick"],reaction:!0,emoji:["//unpkg.com/@waline/emojis@1.1.0/bilibili","//unpkg.com/@waline/emojis@1.1.0/bmoji","//unpkg.com/@waline/emojis@1.1.0/alus","//unpkg.com/@waline/emojis@1.1.0/qq","//unpkg.com/@waline/emojis@1.1.0/tieba","//unpkg.com/@waline/emojis@1.1.0/tw-emoji","//unpkg.com/@waline/emojis@1.1.0/weibo"]};const t=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-MeKfRsNW.js").then(i=>i.W),__vite__mapDeps([]));return e({serverURL:n.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{t as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
