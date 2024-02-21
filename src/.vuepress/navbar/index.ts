87605af0734e729ff8166d3a236f32d556344386
我小叮当 <nikolazhang@163.com>
2024-02-21 15:05:44 +0800

    changes

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -9,80 +9,8 @@ export const zhNavbar = navbar([
         prefix: "/posts/",
         children: [
             {
-                text: "安卓搞机",
-                link: "android/"
-            },
-            {
-                text: "博客搭建相关",
-                link: "blog/"
-            },
-            {
-                text: "数据库",
-                link: "database/"
-            },
-            {
-                text: "elasticsearch",
-                link: "elasticsearch/"
-            },
-            {
-                text: "git",
-                link: "git/"
-            },
-            {
-                text: "golang",
-                link: "golang/"
-            },
-            {
-                text: "hashmap",
-                link: "hashmap/"
-            },
-            {
-                text: "jenkins",
-                link: "jenkins/"
-            },
-            {
-                text: "leetcode",
-                link: "leetcode/"
-            },
-            {
-                text: "linux",
-                link: "linux/"
-            },
-            {
-                text: "markdown",
-                link: "markdown/"
-            },
-            {
-                text: "mybatis",
-                link: "mybatis/"
-            },
-            {
-                text: "设计模式",
-                link: "pattern/"
-            },
-            {
-                text: "springboot",
-                link: "springboot/"
-            },
-            {
-                text: "springcloud",
-                link: "springcloud/"
-            },
-            {
-                text: "线程",
-                link: "thread/"
-            },
-            {
-                text: "工具",
-                link: "tools/"
-            },
-            {
-                text: "虚拟机",
-                link: "vm/"
-            },
-            {
-                text: "日志",
-                link: "log/"
+                text: "文章目录",
+                link: ""
             }
         ],
     },
